7a06e2957030eb11f01e37309b7407476a04ab5f
ssaboorr <sabooransari777@gmail.com>
2021-12-27 23:49:07 +0530

    Completed Styling

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const rootDiv = document.querySelector(".root");
 const search = document.querySelector(".search")
 
-function addData(pokemon_name, pokemon_ability, poke_base, he, mo, src, id) {
+let col = [
+    '#FF5733', '#FF5733', '#DAF7A6', '#DAF7A6', '#FF5733', '#FF5733', '#FFBF00', '#FFBF00', '#FFEA00', '#FFEA00', '#FDDA0D', '#FDDA0D', '#FFFF8F', '#FFFF8F', '#F3E5AB', '#F3E5AB', '#F5DEB3', '#F5DEB3', '#FFFF00', '#FFFF00', '#FFAA33', '#FFAA33',
+]
+
+let col1 = ['#00FFFF', '#F0FFFF', '#89CFF0', '#0000FF', '#7393B3', '#088F8F', '#0096FF', '#5F9EA0', '#0047AB', '#6495ED', '#00FFFF', '#00008B', '#6F8FAF', '#1434A4', '#7DF9FF', '#6082B6', '#00A36C', '#3F00FF', '#5D3FD3', '#ADD8E6', '#191970', '#000080', '#1F51FF', '#A7C7E7', '#CCCCFF', '#B6D0E2', '#96DED1', '#4169E1', '#0F52BA', '#9FE2BF', '#87CEEB', '#4682B4', '#008080', '#40E0D0', '#0437F2', '#40B5AD', '#0818A8', '#00FFFF', '#F0FFFF', '#89CFF0', '#0000FF', '#7393B3', '#088F8F', '#0096FF', '#5F9EA0', '#0047AB', '#6495ED', '#00FFFF', '#00008B', '#6F8FAF', '#1434A4', '#7DF9FF', '#6082B6', '#00A36C', '#3F00FF', '#5D3FD3', '#ADD8E6', '#191970', '#000080', '#1F51FF', '#A7C7E7', '#CCCCFF', '#B6D0E2', '#96DED1', '#4169E1', '#0F52BA', '#9FE2BF', '#87CEEB', '#4682B4', '#008080', '#40E0D0', '#0437F2', '#40B5AD', '#0818A8']
+
+
+// const colors = []
+// while (colors.length < 100) {
+//     do {
+//         var color = Math.floor((Math.random() * 1000000) + 1);
+//     } while (colors.indexOf(color) >= 0);
+//     colors.push("#" + ("000000" + color.toString(16)).slice(-6));
+// }
+// console.log(colors);
+
+const color1 = () => col[Math.floor(Math.random() * 22)]
+const color2 = () => col1[Math.floor(Math.random() * 75)]
+
+
+
+function addData(pokemon_name, pokemon_ability, poke_base, he, mo, src, ) {
     const card = document.createElement("div");
 
     card.setAttribute("class", "card");
@@ -10,7 +31,7 @@ function addData(pokemon_name, pokemon_ability, poke_base, he, mo, src, id) {
     poke_img.setAttribute("src", src);
 
     let poke_name = document.createElement("h2");
-    poke_name.innerText = "Name: " + pokemon_name;
+    poke_name.innerText = "Name: " + pokemon_name.toUpperCase();
 
     let poke_ability = document.createElement("h2");
     poke_ability.innerText = "Ability: " + pokemon_ability;
@@ -24,12 +45,9 @@ function addData(pokemon_name, pokemon_ability, poke_base, he, mo, src, id) {
     let moves = document.createElement("h2");
     moves.innerText = "Move: " + mo;
 
-    let pid = document.createElement("h2");
-    pid.innerText = "Id: " + id;
 
     card.append(poke_img);
     card.append(poke_name);
-    card.append(pid);
 
     card.append(poke_ability);
     card.append(poke_base_exp);
@@ -37,6 +55,9 @@ function addData(pokemon_name, pokemon_ability, poke_base, he, mo, src, id) {
     card.append(moves);
 
     rootDiv.append(card);
+
+    card.style.backgroundImage = `linear-gradient(to bottom, ${color1()}, ${color2()})`
+
 }
 
 let poke_names;
@@ -45,7 +66,6 @@ let poke_base_experience;
 let height;
 let moves;
 let img_src;
-let id;
 
 // console.log(axios)
 let offset = 1;
@@ -56,7 +76,6 @@ for (let i = offset; i <= limit; i++) {
         .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
         .then(({ data }) => {
             poke_names = data.name;
-            poke_id = data.id;
             poke_abilities = data.abilities[0].ability.name;
             poke_base_experience = data.base_experience;
             height = data.height;
@@ -72,7 +91,6 @@ for (let i = offset; i <= limit; i++) {
                 height,
                 moves,
                 img_src,
-                poke_id
             );
         })
         .catch((err) => console.log(err));
@@ -168,14 +186,36 @@ form.addEventListener("submit", (e) => {
                 `
             });
 
-        const buttons = document.querySelector(".buttons");
-        const a = document.createElement("a");
-        a.innerText = "Back";
-        a.setAttribute("href", "index.html");
+        // const buttons = document.querySelector(".buttons");
+        // const a = document.createElement("a");
+        // a.innerText = "Back";
+        // a.setAttribute("href", "index.html");
 
-        const back = document.createElement("button");
-        back.setAttribute("class", "btn-group")
-        back.append(a);
-        buttons.append(back);
+        //     const back = document.createElement("button");
+        //     back.setAttribute("class", "btn-group")
+        //     back.append(a);
+        //     buttons.append(back);
     }
 });
+const buttons = document.querySelector(".buttons");
+const a = document.createElement("a");
+a.innerText = "Back";
+a.setAttribute("href", "index.html");
+const back = document.createElement("button");
+back.setAttribute("class", "btn-group")
+back.append(a);
+buttons.append(back);
+
+
+
+
+
+
+// console.log(color1(), color2())
+
+
+
+
+
+
+// Colors
